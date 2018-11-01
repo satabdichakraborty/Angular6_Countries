9740d87fac960670878899272a81d755b8857978
satabdi.chakraborty <satabdi.d@gmail.com>
2018-11-01 16:37:09 -0400

    Adding Country Service
    Adding Country Service

--- countries/src/app/services/countries.service.ts
+++ countries/src/app/services/countries.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from "@angular/http";
+import { map } from 'rxjs/operators';
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountriesService {
 
-  constructor() { }
+  constructor(private _http:Http) { }
+
+  public getCountries():any{
+    this._http.get("https://restcountries.eu/rest/v2/all").pipe(map(response=>{
+      return response.json();
+    }, error =>{
+      this.handleError(error);
+    }
+    ));
+  }
+
+  public handleError(error){
+    console.error("Error : "+error);
+    return Observable.throw(error || "Internal Server Error");
+  }
 }
